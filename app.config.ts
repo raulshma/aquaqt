25ead2014309a0caf471dbfb23cf742785cbe2f0
raulshma <raulshma@gmail.com>
2026-03-22 14:59:27 +0530

    chore: update version to 0.1.2 in app.config.ts and package.json

--- app.config.ts
+++ app.config.ts
@@ -20,7 +20,7 @@ const splashImage = isDevelopment
 const config: ExpoConfig = {
   name: appName,
   slug: "aquapt",
-  version: "0.1.1",
+  version: "0.1.2",
   orientation: "portrait",
   icon,
   scheme: "aquapt",
